115b29ad2ef70799a63bf10821ade2a62a3dc322
Benjamin M <marsil_b@epitech.eu>
2011-09-20 10:12:00 +0900

    first commit: helloworld

---
+++ TUROTUNA/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  TUROTUNA
+//
+//  Created by mobile6 on 11. 9. 20..
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ TUROTUNA/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  TUROTUNA
+//
+//  Created by mobile6 on 11. 9. 20..
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
