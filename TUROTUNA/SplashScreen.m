06c156aa25f8fe05e3387150a84e6df266a2a227
Pierre SULPICE <pierre.sulpice@epitech.net>
2011-09-27 10:15:20 +0900

    Bump, clean up

--- TUROTUNA/SplashScreen.m
+++ TUROTUNA/SplashScreen.m
@@ -1,13 +1,3 @@
-//
-//  HelloWorldLayer.m
-//  TUROTUNA
-//
-//  Created by mobile6 on 11. 9. 20..
-//  Copyright __MyCompanyName__ 2011. All rights reserved.
-//
-
-
-// Import the interfaces
 #import "SplashScreen.h"
 
 // HelloWorldLayer implementation
@@ -15,58 +5,30 @@
 
 +(CCScene *) scene
 {
-	// 'scene' is an autorelease object.
 	CCScene *scene = [CCScene node];
-	
-	// 'layer' is an autorelease object.
+
 	SplashScreen *layer = [SplashScreen node];
+    [scene addChild: layer];
 	
-	// add layer as a child to scene
-	[scene addChild: layer];
-	
-	// return the scene
 	return scene;
 }
 
-// on "init" you need to initialize your instance
 -(id) init
 {
-	// always call "super" init
-	// Apple recommends to re-assign "self" with the "super" return value
-	if( (self=[super initWithColor:ccc4(255, 255, 255, 255)])) {
+	if( (self=[super initWithColor:ccc4(100, 100, 100, 100)])) {
 
         self.isTouchEnabled = YES;
 
-		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
-		// ask director the the window size
+		CCLabelTTF *label = [CCLabelTTF labelWithString:@"TUROTUNA" fontName:@"Marker Felt" fontSize:64];
 		CGSize size = [[CCDirector sharedDirector] winSize];
-		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
-		// add the label as a child to this Layer
+        label.position =  ccp( size.width /2 , size.height/2 );
 		[self addChild: label];
-        
-        
-        CGSize winSize = [[CCDirector sharedDirector] winSize];
-        CCSprite *player = [CCSprite spriteWithFile:@"Player.png" rect:CGRectMake(0, 0, 27, 40)];
-        player.position = ccp(player.contentSize.width / 2, winSize.height / 2);
-        [self addChild:player];
 	}
 	return self;
 }
 
--(void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-}
-
-
-// on "dealloc" you need to release all your retained objects
 - (void) dealloc
 {
-	// in case you have something to dealloc, do it in this method
-	// in this particular example nothing needs to be released.
-	// cocos2d will automatically release all the children (Label)
-	
-	// don't forget to call "super dealloc"
 	[super dealloc];
 }
 @end
