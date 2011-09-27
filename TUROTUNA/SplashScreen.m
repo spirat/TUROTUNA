5be08f82f8a27cee8b210886cddad1eec081ab21
Pierre SULPICE <pierre.sulpice@epitech.net>
2011-09-27 10:07:18 +0900

    renaming

--- TUROTUNA/SplashScreen.m
+++ TUROTUNA/SplashScreen.m
@@ -8,10 +8,10 @@
 
 
 // Import the interfaces
-#import "HelloWorldLayer.h"
+#import "SplashScreen.h"
 
 // HelloWorldLayer implementation
-@implementation HelloWorldLayer
+@implementation SplashScreen
 
 +(CCScene *) scene
 {
@@ -19,7 +19,7 @@
 	CCScene *scene = [CCScene node];
 	
 	// 'layer' is an autorelease object.
-	HelloWorldLayer *layer = [HelloWorldLayer node];
+	SplashScreen *layer = [SplashScreen node];
 	
 	// add layer as a child to scene
 	[scene addChild: layer];
@@ -33,22 +33,31 @@
 {
 	// always call "super" init
 	// Apple recommends to re-assign "self" with the "super" return value
-	if( (self=[super init])) {
-		
+	if( (self=[super initWithColor:ccc4(255, 255, 255, 255)])) {
+
+        self.isTouchEnabled = YES;
+
 		// create and initialize a Label
 		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
-
 		// ask director the the window size
 		CGSize size = [[CCDirector sharedDirector] winSize];
-	
 		// position the label on the center of the screen
 		label.position =  ccp( size.width /2 , size.height/2 );
-		
 		// add the label as a child to this Layer
 		[self addChild: label];
+        
+        
+        CGSize winSize = [[CCDirector sharedDirector] winSize];
+        CCSprite *player = [CCSprite spriteWithFile:@"Player.png" rect:CGRectMake(0, 0, 27, 40)];
+        player.position = ccp(player.contentSize.width / 2, winSize.height / 2);
+        [self addChild:player];
 	}
 	return self;
 }
+
+-(void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+}
+
 
 // on "dealloc" you need to release all your retained objects
 - (void) dealloc
