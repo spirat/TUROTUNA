4622e6bcb55d0f41c47a06618f33194a7b048fa5
Pierre SULPICE <pierre.sulpice@epitech.net>
2011-10-13 11:37:44 +0900

    Bien parti mais des soucis lors des tests, surement des confusions entre les différentes scènes, j'ajoute des entités addEntityList mais lors de l'update il n'y a rien dedans. Peut être que l'objet est delete lorsque la scène finit...(pourquoi elle finit ??).

--- TUROTUNA/Level.m
+++ TUROTUNA/Level.m
@@ -6,6 +6,7 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+#import "Player.h"
 #import "Level.h"
 
 @implementation Level
@@ -32,12 +33,33 @@
 
 - (void)LoadContent
 {
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"level1.map" ofType:@"txt"];
+    AEntity *entity;
+    int playerType;
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"level1" ofType:@"map"];
     NSString *contents = [NSString stringWithContentsOfFile:path encoding:NSASCIIStringEncoding error:nil];
     NSArray *lines = [contents componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\r\n"]];
+    
     for (NSString* line in lines) {
         if (line.length) {
-            NSLog(@"line: %@", line);
+            NSArray *entry = [line componentsSeparatedByString:@" "];
+            playerType = [[entry objectAtIndex:0] intValue];
+            entity = NULL;
+            
+            if (playerType == PLAYER_TYPE) {
+                entity = [[Player alloc] initWithFile:@"Player.png" rect:CGRectMake(0, 0, 27, 40)];
+                entity.position = ccp([[entry objectAtIndex:1] intValue], [[entry objectAtIndex:2] intValue]);
+            }
+            else if (playerType == TARGET_TYPE) {
+            }
+            else if (playerType == ENEMY_TYPE) {
+            }
+            else if (playerType == OBSTACLE_TYPE) {
+            }
+            
+            if (entity != NULL) {
+                // BUG: chelou...
+                [self addEntity:entity];
+            }
         }
     }
 }
